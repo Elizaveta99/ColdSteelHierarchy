d502f08e3f31fe8c993223591e04dce2ba91696b
Elizaveta <eliz.vitebsk@gmail.com>
2019-09-15 18:36:07 +0300

    abstract factory + builder patterns

--- src/Model/ColdSteel.java
+++ src/Model/ColdSteel.java
@@ -1,11 +1,11 @@
 package Model;
 
 /**
- * Base for factory
+ * Base class for factory
  * @author Elizaveta Rudko
  * @version 1.0.0
  */
-public abstract class ColdSteel {
+public class ColdSteel {
     /**
      * Field - title of cold steel
      */
@@ -14,16 +14,16 @@ public abstract class ColdSteel {
     /**
      * Field - type of cold steel
      */
-    private String type = "coldsteel";
+    //private String type = "coldsteel";
 
-    /**
-     * Fields - determine the presence of a certain part of the weapon
-     */
-    private boolean hasBlade = true;
+    /*private boolean hasBlade = true;
     private boolean hasEdge = true;
     private boolean hasButt = true;
-    private boolean hasHandle = true;
+    private boolean hasHandle = true;*/
 
+    /**
+     * Fields - signs of the weapon
+     */
     private double bladeLength = -1;
     private double buttThickness = -1;
     private double bladeHardness = -1;
@@ -31,10 +31,68 @@ public abstract class ColdSteel {
     private boolean edgeSharpness = false;
 
     /**
-     * Constructor - creates cold steel of certain type
-     * @param _type - type of the cold steel
+     * Builder class for outer class
      */
-    public ColdSteel(String _type, boolean _hasBlade, boolean _hasButt, boolean _hasEdge, boolean _hasHandle,
+    public static class ColdSteelBuilder {
+        private String title = "ColdSteel";
+        //private String type = "coldsteel";
+        /*private boolean hasBlade = true;
+        private boolean hasEdge = true;
+        private boolean hasButt = true;
+        private boolean hasHandle = true;*/
+        private double bladeLength = -1;
+        private double buttThickness = -1;
+        private double bladeHardness = -1;
+        private boolean safetyOfHandle = false;
+        private boolean edgeSharpness = false;
+
+        public ColdSteelBuilder(String title) {
+            this.title = title;
+        }
+
+        public ColdSteelBuilder setBladeLength(double bladeLength) {
+            this.bladeLength = bladeLength;
+            return this;
+        }
+
+        public ColdSteelBuilder setButtThickness(double buttThickness) {
+            this.buttThickness = buttThickness;
+            return this;
+        }
+
+        public ColdSteelBuilder setBladeHardness(double bladeHardness) {
+            this.bladeHardness = bladeHardness;
+            return this;
+        }
+
+        public ColdSteelBuilder setSafetyOfHandle(boolean safetyOfHandle) {
+            this.safetyOfHandle = safetyOfHandle;
+            return this;
+        }
+
+        public ColdSteelBuilder setEdgeSharpness(boolean edgeSharpness) {
+            this.edgeSharpness = edgeSharpness;
+            return this;
+        }
+
+        public ColdSteel build() {
+            return new ColdSteel(this);
+        }
+    }
+    /**
+     * Constructor - creates cold steel of certain type
+     * @param builder - created builder object from the nested class
+     */
+    private ColdSteel(ColdSteelBuilder builder) {
+        this.title = builder.title;
+        this.bladeLength = builder.bladeLength;
+        this.buttThickness = builder.buttThickness;
+        this.bladeHardness = builder.bladeHardness;
+        this.safetyOfHandle = builder.safetyOfHandle;
+        this.edgeSharpness = builder.edgeSharpness;
+    }
+
+    /*public ColdSteel(String _type, boolean _hasBlade, boolean _hasButt, boolean _hasEdge, boolean _hasHandle,
                      double _bladeLength, double _buttThickness, double _bladeHardness,
                      boolean _safetyOfHandle, boolean _edgeSharpness) {
         this.type = _type;
@@ -47,20 +105,12 @@ public abstract class ColdSteel {
         this.buttThickness = _buttThickness;
         this.safetyOfHandle = _safetyOfHandle;
         this.edgeSharpness = _edgeSharpness;
-    }
+    }*/
 
     /**
      * Causes damage
      */
-    public abstract void causeDamage();
-
-    /**
-     * Sets tile of cold steel
-     * @param _title - title
-     */
-    public void setTitle(String _title) {
-        this.title = _title;
-    }
+    public void causeDamage() {};
 
     /**
      * Returns title of cold steel
@@ -69,88 +119,31 @@ public abstract class ColdSteel {
     public String getTitle() {
         return title;
     }
-
-    /**
-     * Returns type of cold steel
-     * @return type
-     */
-    public String getType() {
+    /*public String getType() {
         return type;
-    }
+    }*/
 
-
-    public void setHasBlade(boolean _hasBlade) {
-        this.hasBlade = _hasBlade;
-    }
-    public boolean getHasBlade() {
-        return hasBlade;
-    }
-
-    public void setHasEdge(boolean _hasEdge) {
-        this.hasEdge = _hasEdge;
-    }
-    public boolean getHasEdge() {
-        return hasEdge;
-    }
-
-    public void setHasButt(boolean _hasButt) {
-        this.hasButt = _hasButt;
-    }
-    public boolean getHasButt() {
-        return hasButt;
-    }
-
-    public void setHasHandle(boolean _hasHandle) {
-        this.hasHandle = _hasHandle;
-    }
-    public boolean getHasHandle() {
-        return hasHandle;
-    }
-
-
-
-
-
-    public void setBladeLength(double _bladeLength) {
-        this.bladeLength = _bladeLength;
-    }
     public double getBladeLength() {
         return bladeLength;
     }
 
-    public void setButtThickness(double _buttThickness) {
-        this.buttThickness = _buttThickness;
-    }
     public double getButtThickness() {
         return buttThickness;
     }
 
-
-    public void setBladeHardness(double _bladeHardness) {
-        this.bladeHardness = _bladeHardness;
-    }
     public double getBladeHardness() {
         return bladeHardness;
     }
 
-    public void setSafetyOfHandle(boolean _safetyOfHandle) {
-        this.safetyOfHandle = _safetyOfHandle;
-    }
-    public boolean getSafetyOfHandle() {
-        return safetyOfHandle;
-    }
+    public boolean getSafetyOfHandle() { return safetyOfHandle; }
 
-    public void setEdgeSharpness(boolean _edgeSharpness) {
-        this.edgeSharpness = _edgeSharpness;
-    }
     public boolean getEdgeSharpness() {
         return edgeSharpness;
     }
 
 
     public  boolean isCivilColdSteel() {
-        if (hasBlade && hasEdge && hasButt && hasHandle && bladeLength >= 0.1
-                && buttThickness >= 0.026 && buttThickness <= 0.06 && bladeHardness >= 42 &&
+        if (bladeLength >= 0.1 && buttThickness >= 0.026 && buttThickness <= 0.06 && bladeHardness >= 42 &&
                     edgeSharpness && safetyOfHandle)
             return true;
         else return false;
--- src/Model/Factory.java
+++ src/Model/Factory.java
@@ -9,36 +9,54 @@ import java.lang.Exception;
  */
 public abstract class Factory {
 
-    //private enum Type{ORIENTAL, BALLROOM, FOLK, POP};
+    private enum ColdSteelType{Piercing, Chopping, Stabbing, ShockFragmenting, Crushing};
 
     /**
      * Implementation of factory method
-     *
-     * @author Syrovatnikov Nikita
-     * @version 1.0.0
      */
-
-    // или сюда коллекцию передавать уже? класс для коллекции отдельный
-    public static ColdSteel getClassFromFactory(int id, String _type, boolean _hasBlade, boolean _hasButt, boolean _hasEdge, boolean _hasHandle,
-                                                double _bladeLength, double _buttThickness,
-                                                double _bladeHardness, boolean _safetyOfHandle, boolean _edgeSharpness) throws Exception {
-        switch (id) {
-            case 1:
-                return new PiercingWeapons(_type, _hasBlade, _hasButt, _hasEdge, _hasHandle,
-                        _bladeLength, _buttThickness, _bladeHardness, _safetyOfHandle, _edgeSharpness);
-            case 2:
-                return new ChoppingWeapons(_type, _hasBlade, _hasButt, _hasEdge, _hasHandle,
-                        _bladeLength, _buttThickness, _bladeHardness, _safetyOfHandle, _edgeSharpness);
-            case 3:
-                return new StabbingWeapons(_type, _hasBlade, _hasButt, _hasEdge, _hasHandle,
-                        _bladeLength, _buttThickness, _bladeHardness, _safetyOfHandle, _edgeSharpness);
-            case 4:
-                return new ShockFragmenting(_type, _hasBlade, _hasButt, _hasEdge, _hasHandle,
-                        _bladeLength, _buttThickness, _bladeHardness, _safetyOfHandle, _edgeSharpness);
-            case 5:
-                return new CrushingWeapons(_type, _hasBlade, _hasButt, _hasEdge, _hasHandle,
-                        _bladeLength, _buttThickness, _bladeHardness, _safetyOfHandle, _edgeSharpness);
-                default: throw new Exception();
+    public static ColdSteel createColdSteel(String id, String _title, double _bladeLength, double _buttThickness,
+                                                double _bladeHardness, boolean _safetyOfHandle,
+                                                    boolean _edgeSharpness) throws Exception {
+        ColdSteelType type = ColdSteelType.valueOf(id);
+        ColdSteel coldSteelObject = null;
+        switch (type) {
+            case Piercing:
+                coldSteelObject = new PiercingWeapons.ColdSteelBuilder(_title)
+                        .setBladeLength(_bladeLength)
+                        .setButtThickness(_buttThickness)
+                        .setBladeHardness(_bladeHardness)
+                        .setSafetyOfHandle(_safetyOfHandle)
+                        .setEdgeSharpness(_edgeSharpness).build();
+            case Chopping:
+                coldSteelObject = new ChoppingWeapons.ColdSteelBuilder(_title)
+                        .setBladeLength(_bladeLength)
+                        .setButtThickness(_buttThickness)
+                        .setBladeHardness(_bladeHardness)
+                        .setSafetyOfHandle(_safetyOfHandle)
+                        .setEdgeSharpness(_edgeSharpness).build();
+            case Stabbing:
+                coldSteelObject = new StabbingWeapons.ColdSteelBuilder(_title)
+                        .setBladeLength(_bladeLength)
+                        .setButtThickness(_buttThickness)
+                        .setBladeHardness(_bladeHardness)
+                        .setSafetyOfHandle(_safetyOfHandle)
+                        .setEdgeSharpness(_edgeSharpness).build();
+            case ShockFragmenting:
+                coldSteelObject = new ShockFragmenting.ColdSteelBuilder(_title)
+                        .setBladeLength(_bladeLength)
+                        .setButtThickness(_buttThickness)
+                        .setBladeHardness(_bladeHardness)
+                        .setSafetyOfHandle(_safetyOfHandle)
+                        .setEdgeSharpness(_edgeSharpness).build();
+            case Crushing:
+                coldSteelObject = new CrushingWeapons.ColdSteelBuilder(_title)
+                        .setBladeLength(_bladeLength)
+                        .setButtThickness(_buttThickness)
+                        .setBladeHardness(_bladeHardness)
+                        .setSafetyOfHandle(_safetyOfHandle)
+                        .setEdgeSharpness(_edgeSharpness).build();
+                default: System.err.println("Unknown/unsupported cold steel type.");
         }
+        return coldSteelObject;
     }
 }
